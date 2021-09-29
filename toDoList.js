af2c51e8e5777abd8748ddb3658daa6076ca2bb9
OleksiiHudim <oleksii.h93@gmail.com>
2021-09-29 22:25:05 +0200

    Initial commit

---
+++ toDoList.js
@@ -0,0 +1,53 @@
+const input = document.querySelector('#input')
+const btn = document.querySelector('#btn')
+const result = document.querySelector('#result')
+const total = document.querySelector('#total')
+
+let tasksNumber = 0
+
+input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+    createDeleteElements(input.value)
+    input.value = ''
+     
+    }
+});
+
+btn.addEventListener('click', (e) => {
+    if(input.value === '') return
+    createDeleteElements(input.value)
+    input.value = ''
+})
+// create tasks
+function createDeleteElements(value) {
+    tasksNumber++
+    
+
+
+    const li = document.createElement('li')
+    const btn = document.createElement('button')
+
+    li.className = 'li'
+    li.textContent = value 
+    
+    btn.className = 'btn'
+    btn.textContent = 'Remove'
+    li.appendChild(btn)
+
+    // remove tasks
+    btn.addEventListener('click', (e) => {
+        tasksNumber--
+        total.textContent = tasksNumber
+        result.removeChild(li)
+        
+    })
+
+    li.addEventListener('click', (e) => {
+        li.classList.toggle('li-active')
+    })
+
+    total.textContent = tasksNumber
+
+    result.appendChild(li)
+
+}
